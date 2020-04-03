95ab36fc93266379eed97e314c66dd0462eb329f
mihirkumar02 <mihirkumar02@gmail.com>
2020-04-03 16:43:14 +0530

    Updated footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                         &nbsp;<span className="font-weight-normal">
                         <a className="text-warning" target="blank" rel="noopener noreferrer" href="https://github.com/mihirkumar02"> Mihir Kumar Singh</a>
                         </span>
-                        , using <a className="text-success" target="blank" rel="noopener noreferrer" href="https://covid19-india-adhikansh.herokuapp.com/">Covid-19 INDIA API</a> in collaboration with <a className="text-danger" target="blank" rel="noopener noreferrer" href="https://github.com/HrithikMittal">Adhikansh Mittal</a>.
+                        , using <a className="text-success" target="blank" rel="noopener noreferrer" href="https://covid19-india-adhikansh.herokuapp.com/">Covid-19 INDIA API</a> in collaboration with <a className="text-danger" target="blank" rel="noopener noreferrer" href="https://github.com/HrithikMittal">Adhikansh Mittal</a> for project work.
                         <div>The data has been extracted from </div>
                         <div>the Ministry of Health and Family Welfare, Govt. of India <a href="https://www.mohfw.gov.in/" className="text-primary" target="blank" rel="noopener noreferrer">website</a> and <a className="text-primary" target="blank" rel="noopener noreferrer" href="https://www.who.int/news-room/q-a-detail/q-a-coronaviruses">WHO Official website</a>.</div>
                     </div>
