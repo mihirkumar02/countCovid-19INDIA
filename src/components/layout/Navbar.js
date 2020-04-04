c44b0b1bf4d806de9c5b075b1b5ebbb55aaba10e
mihirkumar02 <mihirkumar02@gmail.com>
2020-04-05 03:46:29 +0530

    Fixed navbar added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-dark bg-light mb-5">
+                <nav className="navbar fixed-top navbar-dark bg-light mb-5">
                   <div className="container">
                         <div className="heading-center">
                             <Link to="/" className="navbar-brand text-dark text-lg bg-light brand-text">
@@ -14,13 +14,13 @@ class Navbar extends Component {
                             </Link>
                         </div>
                         <ul className="navbar-nav text-light d-inline-block">
-                            <li className="nav-item d-inline-block mr-4">
+                            <li className="nav-item d-inline-block mr-3">
                                 <NavLink to="/search" className="text-dark text-lg brand-text" id="logo"><i className="fas fa-search"/> Search</NavLink>
                             </li>
-                            <li className="nav-item d-inline-block mr-4">
+                            <li className="nav-item d-inline-block">
                                 <NavLink to="/statistics" className="text-dark text-lg brand-text" id="logo"><i className="fas fa-map-marker-alt"/> Map</NavLink>
                             </li>
-                            <li className="nav-item d-inline-block mr-4">
+                            <li className="nav-item d-inline-block ml-3">
                                 <NavLink to="/about" className="text-dark text-lg brand-text" id="logo"><i className="fas fa-info-circle"/> About</NavLink>
                             </li>
                         </ul>
